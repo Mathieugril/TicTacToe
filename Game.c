c19e636cf463008fa465dee282940f96f42658db
Mathieugril <mathieugril31@gmail.com>
2026-02-09 19:32:50 +0000

    Basics

--- Game.c
+++ Game.c
@@ -1,7 +1,18 @@
+
+#include <stdio.h>
+
+
+void print_grid();
+
 int main() {
 
+    char choice;
+
+    printf(" === Tic-Tac-Toe ===");
+    prontf(" Chose to play with X or O: ");
+    scanf("%c", &choice);
 
 
-    
+
     return 0;
 }
